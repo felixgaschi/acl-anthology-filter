54dc82ae83134d83f6b703f3db274abc170cd242
Félix Gaschi <felix@posos.fr>
2023-06-22 16:25:25 +0200

    feat: add search command

--- src/environment.rs
+++ src/environment.rs
@@ -1,36 +1,67 @@
 use std::path::PathBuf;
 
-use clap::Parser;
+use clap::{Args, Parser, Subcommand};
 
-#[derive(Parser, Debug)]
-struct Args {
+#[derive(Parser)]
+#[command(author, version, about, long_about = None)]
+#[command(propagate_version = true)]
+pub struct Cli {
+    #[command(subcommand)]
+    pub command: Commands,
+}
+
+#[derive(Subcommand)]
+pub enum Commands {
+    Filter(FilterArgs),
+    Search(SearchArgs),
+}
+
+#[derive(Args)]
+pub struct FilterArgs {
+    #[arg(short, long)]
+    anthology_path: Option<String>,
+
     #[arg(short, long, default_value_t = String::from("."))]
     directory: String,
 
     #[arg(short, long)]
     output_file: Option<String>,
+}
 
+#[derive(Args)]
+pub struct SearchArgs {
     #[arg(short, long)]
     anthology_path: Option<String>,
+
+    #[arg(short, long)]
+    search_string: String,
+
+    #[arg(short, long)]
+    n: Option<usize>,
+}
+
+pub trait ProcessedArgs {
+    type T: Args;
+    fn process(args: &Self::T) -> Self;
 }
 
 #[derive(Debug)]
-pub struct ProcessedArgs {
+pub struct ProcessedFilterArgs {
     pub directory: PathBuf,
     pub anthology_path: Option<PathBuf>,
     pub output_file: PathBuf,
 }
 
-impl ProcessedArgs {
-    pub fn parse() -> ProcessedArgs {
-        let args = Args::parse();
+impl ProcessedArgs for ProcessedFilterArgs {
+    type T = FilterArgs;
 
+    fn process(args: &Self::T) -> Self {
         let directory = PathBuf::from(&args.directory);
-        let anthology_path = match args.anthology_path {
+        let anthology_path = match &args.anthology_path {
             Some(path) => Some(PathBuf::from(path)),
             None => None,
         };
-        let output_file = match args.output_file {
+        let output_file = match &args.output_file {
             Some(path) => PathBuf::from(path),
             None => {
                 let mut p = PathBuf::from(directory.clone());
@@ -38,10 +69,34 @@ impl ProcessedArgs {
                 p
             }
         };
-        ProcessedArgs {
+        return Self {
             directory,
             anthology_path,
             output_file,
-        }
+        };
+    }
+}
+
+#[derive(Debug)]
+pub struct ProcessedSearchArgs {
+    pub anthology_path: PathBuf,
+    pub search_string: String,
+    pub n: Option<usize>,
+}
+
+impl ProcessedArgs for ProcessedSearchArgs {
+    type T = SearchArgs;
+
+    fn process(args: &Self::T) -> Self {
+        let anthology_path = match &args.anthology_path {
+            Some(path) => PathBuf::from(path),
+            None => PathBuf::from("anthology.bib"),
+        };
+
+        return Self {
+            anthology_path,
+            search_string: args.search_string.clone(),
+            n: args.n,
+        };
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ mod bibliography;
 mod citations;
 mod environment;
 mod walkdirectory;
+use clap::Parser;
 use std::fs;
 use std::io::Write;
 use std::{collections::HashSet, fs::File};
@@ -10,14 +11,10 @@ extern crate lazy_static;
 
 use bibliography::{get_anthology_file, parse_bibliography};
 use citations::extract_citation;
-use environment::ProcessedArgs;
+use environment::{Cli, Commands, ProcessedArgs, ProcessedFilterArgs, ProcessedSearchArgs};
 use walkdirectory::filter_walk_by_extension;
 
-fn main() -> std::io::Result<()> {
-    let args = ProcessedArgs::parse();
-
-    println!("{:?}", args);
-
+fn run_filter(args: ProcessedFilterArgs) -> std::io::Result<()> {
     let anthology_path = get_anthology_file(&args.directory, args.anthology_path)?;
     let anthology_contents = fs::read_to_string(anthology_path)?;
     let anthology = parse_bibliography(&anthology_contents);
@@ -39,4 +36,35 @@ fn main() -> std::io::Result<()> {
     }
 
     Ok(())
+}
+
+fn run_search(args: ProcessedSearchArgs) -> std::io::Result<()> {
+    let anthology_contents = fs::read_to_string(args.anthology_path)?;
+    let anthology = parse_bibliography(&anthology_contents);
+
+    let mut count_correct = 0;
+    let lower_search_string = args.search_string.to_lowercase();
+
+    for (_key, val) in anthology.iter() {
+        if val.to_lowercase().contains(&lower_search_string) {
+            println!("{}", val);
+            count_correct = count_correct + 1;
+        }
+        if let Some(_a) = args.n {
+            if args.n.unwrap() <= count_correct {
+                break;
+            }
+        }
+    }
+
+    Ok(())
+}
+
+fn main() -> std::io::Result<()> {
+    let cli = Cli::parse();
+
+    match &cli.command {
+        Commands::Filter(args) => run_filter(ProcessedFilterArgs::process(&args)),
+        Commands::Search(args) => run_search(ProcessedSearchArgs::process(&args)),
+    }
 }
